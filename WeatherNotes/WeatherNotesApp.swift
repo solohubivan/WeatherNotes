8be48497971b167aec9b7239492778c5ca2e7bef
solohubivan <solohubivan@gmail.com>
2026-04-30 01:08:36 +0300

    Initial Commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Ivan Solohub on 30.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
