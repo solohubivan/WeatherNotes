4e0cf1ec134ba9d7891a7a983ad070305e4755e0
solohubivan <solohubivan@gmail.com>
2026-04-30 02:16:35 +0300

    Implement WeatherService and fetch weather using device current location

--- WeatherNotes/Managers/LocationManager.swift
+++ WeatherNotes/Managers/LocationManager.swift
@@ -5,64 +5,6 @@
 //  Created by Ivan Solohub on 30.04.2026.
 //
 
-//import Foundation
-//import CoreLocation
-//
-//@Observable
-//final class LocationManager: NSObject {
-//    
-//    var locationAccessDenied: Bool = false
-//    var onLocationUpdate: ((Double, Double) -> Void)?
-//    private let locationManager = CLLocationManager()
-//    
-//    override init() {
-//        super.init()
-//        setupLocationManager()
-//        requestLocation()
-//    }
-//    
-//    // MARK: - Methods for External Use
-//    func requestLocation() {
-//        locationManager.requestWhenInUseAuthorization()
-//        locationManager.requestLocation()
-//    }
-//    
-//    // MARK: - Configuration
-//    private func setupLocationManager() {
-//        locationManager.delegate = self
-//        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
-//        locationManager.pausesLocationUpdatesAutomatically = false
-//        locationManager.startUpdatingLocation()
-//    }
-//}
-//
-//// MARK: - CLLocationManagerDelegate
-//extension LocationManager: CLLocationManagerDelegate {
-//    
-//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-//        switch status {
-//        case .authorizedAlways, .authorizedWhenInUse:
-//            locationAccessDenied = false
-//            locationManager.requestLocation()
-//        case .denied, .restricted:
-//            locationAccessDenied = true
-//        default:
-//            break
-//        }
-//    }
-//    
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        if let location = locations.last {
-//            onLocationUpdate?(location.coordinate.latitude, location.coordinate.longitude)
-//            locationManager.stopUpdatingLocation()
-//        }
-//    }
-//    
-//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-//        // Intentionally left empty
-//    }
-//}
-
 import Foundation
 import CoreLocation
 
